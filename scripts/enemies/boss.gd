f35cab6cff67d5946b235f5ba677b33064205b27
Reg <niantasan@hotmail.com>
2026-09-20 14:02:18 -0400

    fix rainbow on lvl4 and ositioons of pboss

--- scripts/enemies/boss.gd
+++ scripts/enemies/boss.gd
@@ -223,9 +223,9 @@ var _bullets: Array[Dictionary] = []
 @export var boss_position_duration := 5.0
 @export var boss_fade_duration := 0.5
 
-@export var boss_top_position := Vector2(305.0, 120.0)
-@export var boss_bottom_left_position := Vector2(150.0, 260.0)
-@export var boss_bottom_right_position := Vector2(475.0, 260.0)
+@export var boss_top_position := Vector2(320.0, 120.0)
+@export var boss_bottom_left_position := Vector2(150.0, 250.0)
+@export var boss_bottom_right_position := Vector2(465.0, 250.0)
 
 var _boss_position_timer := 0.0
 var _boss_position_index := 0
