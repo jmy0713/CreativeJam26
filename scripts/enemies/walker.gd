cbe296e7593a594f872e45c6b6d3f8e7f2b1c7a1
Reg <niantasan@hotmail.com>
2026-09-19 23:33:57 -0400

    animation for level4 walker (careful with structure of walker now)

--- scripts/enemies/walker.gd
+++ scripts/enemies/walker.gd
@@ -8,18 +8,30 @@ extends Enemy
 var direction := -1
 
 @onready var ledge_check: RayCast2D = $LedgeCheck
+@onready var animated_sprite: AnimatedSprite2D = get_node_or_null("AnimatedSprite2D")
 
 
 func _ready() -> void:
 	super()
 	direction = start_direction
 
+	if animated_sprite:
+		# Hide only the old ColorRect.
+		body.modulate.a = 0.0
+
+		animated_sprite.play(&"default")
+
 
 func _behave(_delta: float) -> void:
 	if is_on_wall() or (is_on_floor() and not ledge_check.is_colliding()):
 		direction = -direction
+
 	ledge_check.position.x = absf(ledge_check.position.x) * direction
+
 	velocity.x = direction * speed
+
+	if animated_sprite:
+		animated_sprite.flip_h = direction < 0
 
 
 func recall_sample() -> Dictionary:
